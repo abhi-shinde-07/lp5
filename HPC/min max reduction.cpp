b7f8d964c2eef8cf5beef316165a00f5c578fa56
abhi-shinde-07 <133739739+abhi-shinde-07@users.noreply.github.com>
2025-05-12 07:49:22 +0530

    Update min max reduction.cpp

--- HPC/min max reduction.cpp
+++ HPC/min max reduction.cpp
@@ -1,50 +1,50 @@
-/*
-  Windows does not support user defined reductions.
-  This program may not run on MVSC++ compilers for Windows.
-  Please use Linux Environment.[You can try using "windows subsystem for linux"]
-*/
-
-#include<iostream>
-#include<omp.h>
-
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <omp.h> 
 using namespace std;
-int minval(int arr[], int n){
-  int minval = arr[0];
-  #pragma omp parallel for reduction(min : minval)
-    for(int i = 0; i < n; i++){
-      if(arr[i] < minval) minval = arr[i];
+void findMin(const vector<int>& arr) {
+    int min_val = INT_MAX;
+
+    #pragma omp parallel for reduction(min:min_val)
+    for (int i = 0; i < arr.size(); ++i)
+        if (arr[i] < min_val) min_val = arr[i];
+
+    cout << "\nMinimum value = " << min_val << endl;
+}
+void findMax(const vector<int>& arr) {
+    int max_val = INT_MIN;
+
+    #pragma omp parallel for reduction(max:max_val)
+    for (int i = 0; i < arr.size(); ++i)
+        if (arr[i] > max_val) max_val = arr[i];
+
+    cout << "Maximum value = " << max_val << endl;
+}
+void findSumAndAverage(const vector<int>& arr) {
+    int sum = 0;
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < arr.size(); ++i)
+        sum += arr[i];
+    float average = static_cast<float>(sum) / arr.size();
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+}
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    if (n <= 0) {
+        cout << "Invalid number of elements.\n";
+        return 1;
     }
-  return minval;
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+    findMin(arr);
+    findMax(arr);
+    findSumAndAverage(arr);
+    return 0;
 }
 
-int maxval(int arr[], int n){
-  int maxval = arr[0];
-  #pragma omp parallel for reduction(max : maxval)
-    for(int i = 0; i < n; i++){
-      if(arr[i] > maxval) maxval = arr[i];
-    }
-  return maxval;
-}
-
-int sum(int arr[], int n){
-  int sum = 0;
-  #pragma omp parallel for reduction(+ : sum)
-    for(int i = 0; i < n; i++){
-      sum += arr[i];
-    }
-  return sum;
-}
-
-int average(int arr[], int n){
-  return (double)sum(arr, n) / n;
-}
-
-int main(){
-  int n = 5;
-  int arr[] = {1,2,3,4,5};
-  cout << "The minimum value is: " << minval(arr, n) << '\n';
-  cout << "The maximum value is: " << maxval(arr, n) << '\n';
-  cout << "The summation is: " << sum(arr, n) << '\n';
-  cout << "The average is: " << average(arr, n) << '\n';
-  return 0;
-}
